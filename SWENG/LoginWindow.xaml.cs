cfa61fd209ea09a7baddbe83b4406aa25cbf0e51
VeresMarchello <veresmarci@gmail.com>
2019-10-06 21:56:35 +0200

    Login

--- SWENG/LoginWindow.xaml.cs
+++ SWENG/LoginWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,8 +19,9 @@ namespace SWENG
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class LoginWindow : MetroWindow
     {
+        static string Key { get; set; } = "A!9HHhi%XjjYY4YP2@Nob009X";
         public LoginWindow()
         {
             InitializeComponent();
@@ -30,5 +33,70 @@ namespace SWENG
             window.Show();
             this.Close();
         }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamReader reader = System.IO.File.OpenText(@"..\..\..\db1.txt");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                String[] items = line.Split('\t');
+                int myInteger = int.Parse(items[1]);   // Here's your integer.
+
+                // Now let's find the path.
+                string path = null;
+                foreach (string item in items)
+                {
+
+                    path = item;
+                    System.IO.StreamWriter file = new System.IO.StreamWriter(@"..\..\..\db1.txt", true);
+                    file.WriteLine(item);
+                }
+            }
+            //HashPasswordTextBox.Text = Encrypt(PasswordTextBox.Text);
+            //OriginalPasswordTextBox.Text = Decrypt(HashPasswordTextBox.Text);
+        }
+
+        public static string Encrypt(string text)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(Key));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateEncryptor())
+                    {
+                        byte[] textBytes = UTF8Encoding.UTF8.GetBytes(text);
+                        byte[] bytes = transform.TransformFinalBlock(textBytes, 0, textBytes.Length);
+                        return Convert.ToBase64String(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+        }
+
+        public static string Decrypt(string cipher)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(Key));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateDecryptor())
+                    {
+                        byte[] cipherBytes = Convert.FromBase64String(cipher);
+                        byte[] bytes = transform.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                        return UTF8Encoding.UTF8.GetString(bytes);
+
+                    }
+                }
+            }
+        }
+
     }
 }
